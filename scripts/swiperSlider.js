4db5d10ccbd30339aecf51ca8d5d5e41bb4009a2
Tatiana <ganzhinovat@yandex.ru>
2023-03-16 23:56:58 +0300

    feat: add media queries

--- scripts/swiperSlider.js
+++ scripts/swiperSlider.js
@@ -1,6 +1,6 @@
 const slider = new Swiper('.hero__slider', {
   slidesPerView: 2,
-  spaceBetween: 15,
+  spaceBetween: 10,
   loop: true,
   navigation: {
     nextEl: '.hero__slider-btn_next',
@@ -9,6 +9,14 @@ const slider = new Swiper('.hero__slider', {
   autoplay: {
     delay: 3000,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    560: {
+      spaceBetween: 8,
+    },
+  }
 });
 
 export default slider;
