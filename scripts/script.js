0c626cef070866848fe23ab238b4bde7dcc2ff7b
Tatiana <ganzhinovat@yandex.ru>
2023-03-20 22:53:22 +0300

    feat: add modal window by clicking on order button

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
 import slider from './swiperSlider.js';
 import calculator from './calculator.js';
+import modalController from './modal.js';
 
 slider();
 calculator();
+modalController({
+  modal: '.modal',
+  btnOpen: '.js-order',
+  btnClose: '.modal__close',
+});
